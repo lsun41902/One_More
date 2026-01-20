4e4b9851755baa12291ab5bef3c13b99ffacec3f
user <blossom41902@gmail.com>
2026-01-20 17:49:51 +0900

    upload

--- python-server/calc_ai.py
+++ python-server/calc_ai.py
@@ -1,13 +1,306 @@
 import os
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from dotenv import load_dotenv
+import cv2
+from ultralytics import YOLO
+import shutil
+import json
+
+# 제미나이
+from langchain_google_genai import ChatGoogleGenerativeAI
+import google.generativeai as genai
+from PIL import Image, ImageDraw, ImageFont
+from google.api_core import exceptions
+
+load_dotenv()
+
+openai_model = YOLO('yolov10n.pt')
+openai_llm = ChatOpenAI(model="gpt-4o-mini")
+
+genai_api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=genai_api_key)
+
+UPLOAD_DIR = "./uploads"
+
+current_recipes = []
+recipe_titles = set()
+
+model_candidates = [
+    "gemini-2.5-flash-lite",
+    "gemini-2.0-flash",
+    "gemini-3-flash-preview",
+    "gemini-1.5-flash"  # 가장 보수적이고 안전한 선택지
+]
+
+multimodel_candidates = [
+    "gemini-3-flash-preview",    # 2026년 최신, 가장 뛰어난 추론 성능
+    "gemini-2.5-flash",          # 가장 안정적인 고성능 모델
+    "gemini-2.5-flash-lite",     # 속도와 비용 효율 최강
+    "gemini-2.0-flash",          # 구형이지만 여전히 강력한 범용 모델
+    "gemini-1.5-flash"           # 최후의 보루 (하루 1,500회 무료 한도)
+]
 
 
-class llm_question():
+class Text_Question():
     def __init__(self):
-        load_dotenv()
-        os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-        self.llm = ChatOpenAI(model="gpt-4o-mini")
+        pass
 
-    def getQuestion(self,msg):
-        return self.llm.invoke(msg)
+    def get_recipe(self, msg):
+        return openai_llm.invoke(msg)
+
+
+class Analyze():
+    # region 생성자
+    def __init__(self, images=None, user_id='user1'):
+        self.image_path = os.path.join(UPLOAD_DIR, user_id)
+        os.makedirs(self.image_path, exist_ok=True)
+        self.saved_file_paths = []
+        if images:
+            for image in images:
+                file_path = os.path.join(self.image_path, image.filename)
+                image.file.seek(0)
+                # 2. 파일을 로컬 디스크에 저장 (Option B 구현)
+                with open(file_path, "wb") as buffer:
+                    shutil.copyfileobj(image.file, buffer)
+                self.saved_file_paths.append(file_path)
+    # endregion
+
+    # region Genai 모델
+    def get_text_model(self,prompt):
+        for model_name in model_candidates:
+            try:
+                # 모델 선언 및 호출 (LangChain 기준)
+                print("현재 모델 이름:",model_name)
+                llm = ChatGoogleGenerativeAI(
+                    model=model_name,
+                    google_api_key=genai_api_key,
+                    model_kwargs={"response_mime_type": "application/json"}
+                )
+                return llm.invoke(prompt)
+            except exceptions.ResourceExhausted:
+                # 횟수 초과(Quota) 오류 발생
+                print(f"현재 모델 이름:{model_name} 한도 초과! 모델로 전환하여 다시 시도합니다.")
+                pass
+            except Exception as e:
+                if "429" in str(e) or "quota" in str(e).lower():
+                    print(f"{model_name} 한도 초과, 다음 모델로 시도합니다...")
+                    continue
+                else:
+                    raise e
+        print("모든 모델의 할당량이 소진되었습니다.")
+        return None
+
+    def get_image_model(self,content):
+        for model_name in multimodel_candidates:
+            try:
+                # 모델 선언 및 호출 (LangChain 기준)
+                print("현재 모델 이름:",model_name)
+                genai_model = genai.GenerativeModel(model_name)
+                return genai_model.generate_content(
+                    content,
+                    generation_config={"response_mime_type": "application/json"})
+            except exceptions.ResourceExhausted:
+                # 횟수 초과(Quota) 오류 발생
+                print(f"현재 모델 이름:{model_name} 한도 초과! 모델로 전환하여 다시 시도합니다.")
+                pass
+            except Exception as e:
+                if "429" in str(e) or "quota" in str(e).lower():
+                    print(f"{model_name} 한도 초과, 다음 모델로 시도합니다...")
+                    continue
+                else:
+                    raise e
+        print("모든 모델의 할당량이 소진되었습니다.")
+        return None
+
+    # endregion
+
+    #region 몇가지만 재료가 더 있으면!
+    def get_more_something(self, preference, ingredients, spice):
+        prompt = f"""
+                음.. 혹시 지금 내가 가지고 있는 재료들과 1~5개 정도의 재료 또는 조미료가 있다면 더 맛있는 음식이 가능할까?
+                나는 {preference} 이런 취향을 가지고 있고, {ingredients} 이런 재료들고 {spice} 이런 조미료를 가지고있어,
+                레시피는 인터넷에서 한국식, 일본식, 서양식 이였으면 좋겠고, 사람들이 이미 만든적이 
+                있는 레시피를 기반으로 3개 추천해줬으면 좋겠어. 꼭 3가지를 찾지 못해도 상관없어. 적어도 3가지를 추천해줘. 
+                근거가 없는 요리는 추천하지 않도록 조심해하고! recipe는 손질 방법과, 정량 기준, 조리에 대한 상세한 시간을 꼭 언급해줘.
+                필요한 재료들은 more에 넣어줘!
+                title,ingredients,more,summary,recipe 는 한글로 알려줘, 결과는 반드시 JSON 배열로만 응답해줘.
+                응답은 반드시 아래 JSON 형식을 지켜줘:
+
+              {{
+                "title": "요리 이름 (예: 김치볶음밥)",
+                "ingredients": ["재료1", "재료2", "재료3"],
+                "more":["필요한재료1","필요한재료2","필요한재료3"],
+                "summary": "해당 요리에 대한 50자 이내의 간략한 설명",
+                "recipe": ["step1: 재료 손질 방법과 재료의 정량 기준을 알려주기(밥 숫가락으로 한숟갈, 티 스푼으로 한 숟갈, 종이컵 반컵)"],["step2: 냄비에 넣고 강불로 5분간 끓여주세요"],
+                "tip":["tip1: 닭을 손질할때 우유를 넣어서 잡내를 없애면 좋아요!","tip2: 일반 소금 대신 맛소금을 사용하면 더 맛있어요!"]
+              }},
+              {{
+                "title": "요리 이름 (예: 김치볶음밥)",
+                "ingredients": ["재료1", "재료2", "재료3"],
+                "more":["필요한재료1","필요한재료2","필요한재료3"],
+                "summary": "해당 요리에 대한 50자 이내의 간략한 설명",
+                "recipe": ["step1: 재료 손질 방법과 재료의 정량 기준을 알려주기(밥 숫가락으로 한숟갈, 티 스푼으로 한 숟갈, 종이컵 반컵)"],["step2: 냄비에 넣고 강불로 5분간 끓여주세요"],
+                "tip":["tip1: 닭을 손질할때 우유를 넣어서 잡내를 없애면 좋아요!","tip2: 일반 소금 대신 맛소금을 사용하면 더 맛있어요!"]
+              }}
+
+                """
+
+        response = self.get_text_model(prompt)
+        result_json = json.loads(response.content)
+        self.save_recipe(result_json)
+        return result_json
+    #endregion
+
+    # region 레시피 추천
+    def get_recipe(self, preference, ingredients, spice):
+        current_recipes = []
+        prompt = f"""
+                나는 지금 배가고파 요리 레시피를 대신좀 찾아줘. 
+                나는 {preference} 이런 취향을 가지고 있고, {ingredients} 이런 재료들고 {spice} 이런 조미료를 가지고있어,
+                다른 재료는 없으니 참고해. 레시피는 인터넷에서 한국식, 일본식, 서양식 이였으면 좋겠고, 사람들이 이미 만든적이 
+                있는 레시피를 기반으로 3개 추천해줬으면 좋겠어. 꼭 3가지를 찾지 못해도 상관없어. 적어도 3가지를 추천해줘. 
+                근거가 없는 요리는 추천하지 않도록 조심해하고! recipe는 손질 방법과, 정량 기준, 조리에 대한 상세한 시간을 꼭 언급해줘.
+                title,ingredients,summary,recipe 는 한글로 알려줘, 결과는 반드시 JSON 배열로만 응답해줘.
+                응답은 반드시 아래 JSON 형식을 지켜줘:
+            
+              {{
+                "title": "요리 이름 (예: 김치볶음밥)",
+                "ingredients": ["재료1", "재료2", "재료3"],
+                "summary": "해당 요리에 대한 50자 이내의 간략한 설명",
+                "recipe": ["step1: 재료 손질 방법과 재료의 정량 기준을 알려주기(밥 숫가락으로 한숟갈, 티 스푼으로 한 숟갈, 종이컵 반컵)"],["step2: 냄비에 넣고 강불로 5분간 끓여주세요"],
+                "tip":["tip1: 닭을 손질할때 우유를 넣어서 잡내를 없애면 좋아요!","tip2: 일반 소금 대신 맛소금을 사용하면 더 맛있어요!"]
+              }},
+              {{
+                "title": "요리 이름 (예: 김치볶음밥)",
+                "ingredients": ["재료1", "재료2", "재료3"],
+                "summary": "해당 요리에 대한 50자 이내의 간략한 설명",
+                "recipe": ["step1: 재료 손질 방법과 재료의 정량 기준을 알려주기(밥 숫가락으로 한숟갈, 티 스푼으로 한 숟갈, 종이컵 반컵)"],["step2: 냄비에 넣고 강불로 5분간 끓여주세요"],
+                "tip":["tip1: 닭을 손질할때 우유를 넣어서 잡내를 없애면 좋아요!","tip2: 일반 소금 대신 맛소금을 사용하면 더 맛있어요!"]
+              }}
+            
+                """
+
+        response = self.get_text_model(prompt)
+        result_json = json.loads(response.content)
+        self.save_recipe(result_json)
+        return result_json
+
+    def get_another_recipe(self, preference, ingredients,spice):
+        prompt = f"""
+                방금 추천한 {recipe_titles} 이 레시피들 말고 다른 레시피를 원해. 중복되지 않게 조심해. 
+                나는 {preference} 이런 취향을 가지고 있고, {ingredients} 이런 재료들고 {spice} 이런 조미료를 가지고있어, 다른 재료는 없으니 참고해.
+                레시피는 인터넷에서 한국식, 일본식, 서양식 이였으면 좋겠고, 사람들이 이미 만든적이 있는 레시피를 기반으로 3개 추천해줬으면 좋겠어. 
+                꼭 3가지를 찾지 못해도 상관없어. 적어도 3가지를 추천해줘. 근거가 없는 요리는 추천하지 않도록 조심해하고! 
+                recipe는 손질 방법과, 정량 기준, 조리에 대한 상세한 시간을 꼭 언급해줘.
+                title,ingredients,summary,recipe 는 한글로 알려줘, 결과는 반드시 JSON 배열로만 응답해줘.
+                응답은 반드시 아래 JSON 형식을 지켜줘:
+            
+              {{
+                "title": "요리 이름 (예: 김치볶음밥)",
+                "ingredients": ["재료1", "재료2", "재료3"],
+                "summary": "해당 요리에 대한 50자 이내의 간략한 설명",
+                "recipe": ["step1: 재료 손질 방법과 재료의 정량 기준을 알려주기(밥 숫가락으로 한숟갈, 티 스푼으로 한 숟갈, 종이컵 반컵)"],["step2: 냄비에 넣고 강불로 5분간 끓여주세요"],
+                "tip":["tip1: 닭을 손질할때 우유를 넣어서 잡내를 없애면 좋아요!","tip2: 일반 소금 대신 맛소금을 사용하면 더 맛있어요!"]
+              }},
+              {{
+                "title": "요리 이름 (예: 김치볶음밥)",
+                "ingredients": ["재료1", "재료2", "재료3"],
+                "summary": "해당 요리에 대한 50자 이내의 간략한 설명",
+                "recipe": ["step1: 재료 손질 방법과 재료의 정량 기준을 알려주기(밥 숫가락으로 한숟갈, 티 스푼으로 한 숟갈, 종이컵 반컵)"],["step2: 냄비에 넣고 강불로 5분간 끓여주세요"],
+                "tip":["tip1: 닭을 손질할때 우유를 넣어서 잡내를 없애면 좋아요!","tip2: 일반 소금 대신 맛소금을 사용하면 더 맛있어요!"]
+              }}"""
+
+        response = self.get_text_model(prompt)
+        result_json = json.loads(response.content)
+        self.save_recipe(result_json)
+        return result_json
+
+    def save_recipe(self, data):
+        for recipe in data:
+            title = recipe['title']
+            if title not in recipe_titles:
+                current_recipes.append(recipe)  # 리스트에 추가
+                recipe_titles.add(title)  # set에 제목 등록
+                print("이미 추천한 레시피:",title)
+
+    # endregion
+
+    # region 영수증 분석
+    def calc_image_receipt(self):
+        imgs = [Image.open(path) for path in self.saved_file_paths]
+        num_images = len(imgs)
+        prompt = f"""
+            총 {num_images}장의 사진이 있어. 각 사진별로 무엇을 몇개를 구매했는지 탐색하고 알려줘.
+            ingredients의 재료는 한글로 알려줘, 결과는 반드시 image_index를 포함한 JSON 배열로만 응답해줘.
+            응답은 반드시 아래 JSON 형식을 지켜줘:
+        {{
+        "ingredients": ["재료1", "재료2"],
+        "detections": [{{"label": "Apple", "box": [100, 150, 400, 450],"count":2개}},{{"label": "Milk", "box": [500, 600, 900, 800],"count":2개}}]
+        }}
+            """
+        content = [prompt] + imgs
+        response = self.get_image_model(content)
+        return json.loads(response.text)
+
+    # endregion
+
+    # region 이미지 분석
+    def calc_image(self):
+        imgs = [Image.open(path) for path in self.saved_file_paths]
+        num_images = len(imgs)
+        prompt = f"""
+            총 {num_images}장의 사진이 있어. 각 사진별로 식재료를 탐지하고 좌표를 알려줘.
+            ingredients의 재료는 한글로 알려줘, 결과는 반드시 image_index를 포함한 JSON 배열로만 응답해줘.
+            그리고 되도록이면 이미지에서 분석한 해당 재료를 센는 단위로 인식한 수량(계량정도)를 같이 알려줘
+            응답은 반드시 아래 JSON 형식을 지켜줘:
+        {{
+        "ingredients": ["재료1", "재료2"],
+        "detections": [{{"label": "Apple", "box": [100, 150, 400, 450],"weighing":"1개"}},{{"label": "Milk", "box": [500, 600, 900, 800],"weighing":"1컵"}}]
+        }}
+            """
+        content = [prompt] + imgs
+        response = self.get_image_model(content)
+        return self.draw_boxes(json.loads(response.text))
+
+    def draw_boxes(self, data):
+        images_results = {}
+
+        for image_result in data:  # data 자체가 리스트이므로 바로 순회합니다.
+            idx = image_result['image_index']
+            # 해당 이미지의 detections 리스트를 가져와 저장합니다.
+            images_results[idx] = image_result['detections']
+
+        # 1. 저장된 경로 리스트를 순회
+        for i, path in enumerate(self.saved_file_paths):
+            # 이미지 열기
+            img = Image.open(path)
+            draw = ImageDraw.Draw(img)
+
+            # 2. 폰트 설정
+            try:
+                font = ImageFont.truetype("arial.ttf", 20)
+            except:
+                font = ImageFont.load_default()
+
+            # 3. 각 탐지 결과에 대해 그리기 (해당 이미지에 맞는 데이터인지 확인 필요)
+            for det in images_results[i]:
+                # 주의: 여러 장의 이미지 결과가 섞여 있다면
+                # det에 있는 image_index와 현재 path의 순서가 맞는지 체크 로직이 필요할 수 있습니다.
+                label = det['label']
+                box = det['box']
+                draw.rectangle([box[1], box[0], box[3], box[2]], outline="red", width=5)
+                draw.text((box[1], box[0] - 25), label, fill="red", font=font)
+
+            # 4. 파일명 추출 및 저장 경로 생성
+            # 예: D:/data/food.jpg -> 디렉토리: D:/data, 파일명: food, 확장자: .jpg
+            directory, filename = os.path.split(path)
+            name, ext = os.path.splitext(filename)
+
+            # 새로운 저장 경로: D:/data/food_label.jpg
+            output_path = os.path.join(directory, f"{name}_label{ext}")
+
+            img.save(output_path)
+            print(f"저장 완료: {output_path}")
+
+        return data
+    # endregion
--- python-server/one_more_server.py
+++ python-server/one_more_server.py
@@ -1,53 +1,116 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Annotated
 import calc_ai
 from fastapi import FastAPI, File, UploadFile
-import shutil
-import os
 
-
+#기본 셋팅
 app = FastAPI()
-process = calc_ai.llm_question()
+
 # 이미지를 저장할 폴더 생성
 UPLOAD_DIR = "./uploads"
 
+#클래스 생성
 class Question(BaseModel):
     message: str
 
-class IngredientRequest(BaseModel):
+class Preferences_Ingredient(BaseModel):
+    preferences: List[str]
     ingredients: List[str]
+    spices: List[str]
 
-#텍스트 질문을 받으면 답변해주기
-@app.post("/ai")
-async def ask_ai(item:Question):
-    print(f"Spring 친구가 보낸 메시지:{item.message}")
-    result = process.getQuestion(item.message)
-    return {"reply": result}
 
-@app.post("/analyze")
-async def analyze(request: IngredientRequest):
-    print(f"옆자리 자바 서버에서 온 재료: {request.ingredients}")
-    return {"status": "success", "message": "잘 받았습니다!"}
+#region 텍스트 분석
+@app.post("/analyze-text",
+    tags=["분석 - 텍스트"],
+    summary="텍스트 재료 분석",
+    description="텍스트로 전달 받은 핵심 재료들을 분석하여 레시피 추천.")
+async def analyze(request: Preferences_Ingredient):
+    process_text = calc_ai.Analyze()
+    result = process_text.get_recipe(request.preferences,request.ingredients,request.spices)
+    return {"result": result}
+#endregion
 
-@app.post("/analyze-image")
+#region 다른 레시피 추천받기
+@app.post("/another-recipe",
+    tags=["분석 - 텍스트"],
+    summary="다른 레시피 추천",
+    description="방금전에 말한 레시피가 아닌 다른 레시피 추천 받기.")
+async def analyze(request: Preferences_Ingredient):
+    process_text = calc_ai.Analyze()
+    result = process_text.get_another_recipe(request.preferences,request.ingredients,request.spices)
+    return {"result": result}
+#endregion
+
+#region 만약 다른 재료가 더 있다면!
+@app.post("/more-somthing",
+    tags=["분석 - 텍스트"],
+    summary="추가 재료가 있다면",
+    description="추가로 재료가 더 있다면 이런 음식도 가능해요!")
+async def analyze(request: Preferences_Ingredient):
+    process_text = calc_ai.Analyze()
+    result = process_text.get_more_something(request.preferences,request.ingredients,request.spices)
+    return {"result": result}
+#endregion
+
+#region 영수증 사진 분석
+#이미지로 입력받은 재료 분석후 레시피 추천하기
+@app.post(
+    "/analyze-image-receipt",
+    tags=["분석 - 영수증"],
+    summary="영수증 재료 분석",
+    description="영수증에서 재료들을 분석하여 재료를 확인."
+)
 async def analyze_image(file: UploadFile = File(...)):
-    # 1. 파일 저장 경로 설정
-    print(f"접속됨")
-    file_path = os.path.join(UPLOAD_DIR, file.filename)
-    os.makedirs(UPLOAD_DIR, exist_ok=True)
+    # 단일 파일도 리스트로 감싸서 보내면 Image_Material을 공통으로 쓸 수 있네.
+    process_image = calc_ai.Analyze([file])
+    result = process_image.calc_image_receipt()
+    return {"result": result}
 
-    # 2. 파일을 로컬 디스크에 저장 (Option B 구현)
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+@app.post("/analyze-multiple-receipt",
+          tags=["분석 - 영수증"],
+          summary="영수증 재료 분석",
+          description="영수증에서 재료들을 분석하여 재료를 확인."
+          )
+async def analyze_multiple_images(
+    files: Annotated[List[UploadFile], File(description="여러 개의 이미지 파일을 선택하세요")]
+):
+    # 3. AI 클래스에 이미지 리스트 전달
+    process_image = calc_ai.Analyze(files)
+    result = process_image.calc_image_receipt()
+    return {"result": result}
+#endregion
 
-    print(f"이미지 수신 완료: {file.filename} 저장됨")
+#region 음식 사진 분석
+#이미지로 입력받은 재료 분석후 레시피 추천하기
+@app.post(
+    "/analyze-image",
+    tags=["분석 - 이미지"],
+    summary="이미지 재료 분석",
+    description="이미지로 전달 받은 재료들을 분석하여 재료를 확인."
+)
+async def analyze_image(file: UploadFile = File(...)):
+    # 단일 파일도 리스트로 감싸서 보내면 Image_Material을 공통으로 쓸 수 있네.
+    process_image = calc_ai.Analyze([file])
+    result = process_image.calc_image()
+    return {"result": result}
 
-    # 3. 나중에는 여기서 YOLO 모델을 돌리게 됩니다.
-    return {
-        "status": "success",
-        "filename": file.filename,
-        "message": "이미지가 서버에 성공적으로 저장되었습니다."
-    }
+@app.post("/analyze-multiple",
+          tags=["분석 - 이미지"],
+          summary="이미지 재료 분석",
+          description="이미지로 전달 받은 재료들을 분석하여 재료를 확인."
+          )
+async def analyze_multiple_images(
+    files: Annotated[List[UploadFile], File(description="여러 개의 이미지 파일을 선택하세요")]
+):
+    # 3. AI 클래스에 이미지 리스트 전달
+    process_image = calc_ai.Analyze(files)
+    result = process_image.calc_image()
+    return {"result": result}
+#endregion
+
+
+
+
 
 #메인
 if __name__ == "__main__":
